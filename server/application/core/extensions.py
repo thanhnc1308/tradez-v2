6ad03e5611bb665274c3bf9151b1bd40ccc6ff0f
thanhnc <thanhnc138@mail.com>
2022-02-20 18:50:33 +0700

    add worker example

--- server/application/core/extensions.py
+++ server/application/core/extensions.py
@@ -3,6 +3,24 @@ from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 from flask_caching import Cache
 from flask_cors import CORS
+from celery import Celery
+
+
+def make_celery(app):
+    celery = Celery(
+        app.import_name,
+        backend=app.config['CELERY_RESULT_BACKEND'],
+        broker=app.config['CELERY_BROKER_URL']
+    )
+    celery.conf.update(app.config)
+
+    class ContextTask(celery.Task):
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return self.run(*args, **kwargs)
+
+    celery.Task = ContextTask
+    return celery
 
 
 db = SQLAlchemy()
---
+++ server/application/workers/notification_worker/notification_worker.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from application.core.extensions import make_celery
+
+flask_app = Flask(__name__)
+flask_app.config.update(
+    CELERY_BROKER_URL='redis://localhost:6379',
+    CELERY_RESULT_BACKEND='redis://localhost:6379'
+)
+celery = make_celery(flask_app)
+
+
+@celery.task()
+def add_together(a, b):
+    return a + b
